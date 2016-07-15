6ee9733a5f76f9dd4ae29176f888f40b22609cce
Aslak Knutsen <aslak@4fs.no>
2016-07-15 11:56:48 +0200

    TEMP Models test

--- models/models_test.go
+++ models/models_test.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"testing"
 
-	"golang.org/x/net/context"
-
-	"github.com/almighty/almighty-core/migration"
-	"github.com/almighty/almighty-core/models"
 	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"golang.org/x/net/context"
 )
 
 func TestMigration(t *testing.T) {
@@ -17,27 +15,35 @@ func TestMigration(t *testing.T) {
 		panic(err)
 	}
 	// Migrate the schema
-	migration.Perform(db)
+	//migration.Perform(db)
 
 	//createInitialSetup(db)
 	//findIdentity(db)
 	//findTeam(db)
-	findTeamByIdentity(db)
+	//findTeamByIdentity(db)
+
+	userDB := NewUserDB(db)
+	users, err := userDB.Query(ByEmails([]string{"aslak@4fs.no"}), WithIdentity())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(users[0])
+
 }
 
 func findTeamByIdentity(db *gorm.DB) {
-	var user models.User
-	db.Preload("Identity").Where(&models.User{Email: "aslak@4fs.no"}).First(&user)
+	var user User
+	db.Preload("Identity").Where(&User{Email: "aslak@4fs.no"}).First(&user)
 
-	var teams []models.Team
+	var teams []Team
 	db.Debug().Model(&user.Identity).Association("Identities").Find(&teams)
 	fmt.Println("Teams: ", teams)
 }
 
 func findTeam(db *gorm.DB) {
 
-	var team models.Team
-	db.Debug().Preload("Permissions").Preload("Projects").Preload("Identities").Where(&models.Team{Name: "Core"}).First(&team)
+	var team Team
+	db.Debug().Preload("Permissions").Preload("Projects").Preload("Identities").Where(&Team{Name: "Core"}).First(&team)
 	fmt.Println("Permissions: ", team.Permissions)
 	fmt.Println("Projects: ", team.Projects)
 
@@ -45,46 +51,51 @@ func findTeam(db *gorm.DB) {
 
 func findIdentity(db *gorm.DB) {
 
-	var user models.User
-	db.Debug().Preload("Identity.Teams").Where(&models.User{Email: "aslak@4fs.no"}).First(&user)
+	var user User
+	db.Debug().Preload("Identity.Teams").Where(&User{Email: "aslak@4fs.no"}).First(&user)
 	fmt.Println("A: ", user.Identity)
 }
 
 func createInitialSetup(db *gorm.DB) {
-	users := models.NewUserDB(*db)
-	identities := models.NewIdentityDB(*db)
-	projects := models.NewProjectDB(*db)
-	teams := models.NewTeamDB(*db)
-	permissions := models.NewPermissionDB(*db)
+	users := NewUserDB(db)
+	identities := NewIdentityDB(db)
+	projects := NewProjectDB(db)
+	teams := NewTeamDB(db)
+	permissions := NewPermissionDB(db)
 	ctx := context.Background()
 
-	me, err := identities.Add(ctx, &models.Identity{FullName: "Aslak Knutsen"})
+	me := &Identity{FullName: "Aslak Knutsen"}
+	err := identities.Add(ctx, me)
 	if err != nil {
 		panic(err)
 	}
 
-	firstEmail, err := users.Add(ctx, &models.User{Email: "aslak@4fs.no", Identity: *me})
+	firstEmail := &User{Email: "aslak@4fs.no", Identity: *me}
+	err = users.Add(ctx, firstEmail)
 	if err != nil {
 		panic(err)
 	}
 
-	project, err := projects.Add(ctx, &models.Project{Name: "ALMighty"})
+	project := &Project{Name: "ALMighty"}
+	err = projects.Add(ctx, project)
 	if err != nil {
 		panic(err)
 	}
 
-	var storedPermissions []models.Permission
+	var storedPermissions []Permission
 
 	permissionStr := []string{"create.workitem", "read.workitem", "update.workitem", "delete.workitem"}
 	for _, permission := range permissionStr {
-		perm, err := permissions.Add(ctx, &models.Permission{Name: permission})
+		perm := &Permission{Name: permission}
+		err := permissions.Add(ctx, perm)
 		if err != nil {
 			panic(err)
 		}
 		storedPermissions = append(storedPermissions, *perm)
 	}
 
-	team, err := teams.Add(ctx, &models.Team{Name: "Core", Permissions: storedPermissions, Identities: []models.Identity{*me}, Projects: []models.Project{*project}})
+	team := &Team{Name: "Core", Permissions: storedPermissions, Identities: []Identity{*me}, Projects: []Project{*project}}
+	err = teams.Add(ctx, team)
 	if err != nil {
 		panic(err)
 	}
